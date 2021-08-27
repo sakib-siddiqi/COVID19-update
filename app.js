5f40b8c1c3328178b8079451634d4de3ef8aaddb
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-08-27 21:52:52 +0600

    added js

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
+let D_C_W = document.querySelector('.D_C_W');
+let A_C_W = document.querySelector('.A_C_W');
+let R_W = document.querySelector('.R_W');
+let D_D_W = document.querySelector('.D_D_W');
+let T_D_W = document.querySelector('.T_D_W');
+
+let D_C_BD = document.querySelector('.D_C_BD');
+let A_C_BD = document.querySelector('.A_C_BD');
+let R_BD = document.querySelector('.R_BD');
+let D_D_BD = document.querySelector('.D_D_BD');
+let T_D_BD = document.querySelector('.T_D_BD');
+
+const calculatetor = (w, bd) => {
+    D_C_W.innerText=w.todayCases;
+    A_C_W.innerText=w.active;
+    R_W.innerText=w.recovered;
+    D_D_W.innerText=w.todayDeaths;
+    T_D_W.innerText=w.deaths;
+
+    D_C_BD.innerText=bd.todayCases;
+    A_C_BD.innerText=bd.active;
+    R_BD.innerText=bd.recovered;
+    D_D_BD.innerText=bd.todayDeaths;
+    T_D_BD.innerText=bd.deaths;
+}
 async function covidUpdate() {
     let fetching = await fetch('https://coronavirus-19-api.herokuapp.com/countries');
     let convertToJson = fetching.json();
     let response = await convertToJson;
+    console.log(response)
     let bdResult = response[27];
     let worldResult = response[0];
-    console.log(bdResult,worldResult)
+    calculatetor(worldResult, bdResult);
 }
 covidUpdate();
